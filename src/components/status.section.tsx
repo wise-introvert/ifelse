1f52a90e5b85b5603afcac737474235c87e833d0
Fardeen Panjwani <fardeen.wi.panjwani@gmail.com>
2023-05-31 14:07:25 -0400

    feat: full migrate StatusSection component's UI

--- src/components/status.section.tsx
+++ src/components/status.section.tsx
@@ -1,10 +1,10 @@
 import { FC, ReactElement, useState } from "react";
 import {
   View,
+  StyleSheet,
   Text,
   TouchableOpacity,
   GestureResponderEvent,
-  Platform,
 } from "react-native";
 import { Feather } from "@expo/vector-icons";
 
@@ -19,42 +19,64 @@ export const StatusSection: FC<StatusSectionProps> = ({
 }: StatusSectionProps): ReactElement<StatusSectionProps> => {
   const colors: Colors = useColors();
   const [activeTab, setActiveTab] = useState<string>("recent");
+  const styles = StyleSheet.create({
+    container: {
+      height: 48,
+      marginTop: 20,
+      marginHorizontal: 8,
+      flexDirection: "row",
+      alignItems: "center",
+      justifyContent: "space-evenly",
+    },
+    tabButton: {
+      flex: 1,
+      marginHorizontal: 4,
+      height: "100%",
+      alignItems: "center",
+      justifyContent: "center",
+    },
+    tabButtonText: {
+      fontSize: 16,
+      fontWeight: "400",
+    },
+    filterButton: {
+      flex: 0.5,
+      marginHorizontal: 4,
+      height: "100%",
+      alignItems: "center",
+      justifyContent: "center",
+    },
+    statusCardsContainer: {
+      flexDirection: "row",
+      marginTop: 12,
+      marginHorizontal: 8,
+      justifyContent: "space-between",
+    },
+  });
 
   return (
     <View>
-      <View
-        style={{
-          height: 48,
-          marginTop: 20,
-          marginHorizontal: 8,
-          flexDirection: "row",
-          alignItems: "center",
-          justifyContent: "space-evenly",
-        }}
-      >
+      <View style={styles.container}>
         <TouchableOpacity
           onPress={(e: GestureResponderEvent): void => {
             e.preventDefault();
             setActiveTab("recent");
           }}
-          style={{
-            flex: 1,
-            marginHorizontal: 4,
-            height: "100%",
-            ...(activeTab == "recent" && {
-              borderRadius: Platform.OS == "web" ? 80 : ("50%" as any),
+          style={[
+            styles.tabButton,
+            activeTab === "recent" && {
+              borderRadius: 80,
               backgroundColor: colors.blue,
-            }),
-            alignItems: "center",
-            justifyContent: "center",
-          }}
+            },
+          ]}
         >
           <Text
-            style={{
-              fontSize: 16,
-              fontWeight: "400",
-              color: activeTab == "recent" ? colors.white : colors.bluegray,
-            }}
+            style={[
+              styles.tabButtonText,
+              {
+                color: activeTab === "recent" ? colors.white : colors.bluegray,
+              },
+            ]}
           >
             Recent
           </Text>
@@ -64,24 +86,22 @@ export const StatusSection: FC<StatusSectionProps> = ({
             e.preventDefault();
             setActiveTab("all_groups");
           }}
-          style={{
-            flex: 1,
-            marginHorizontal: 4,
-            height: "100%",
-            ...(activeTab == "all_groups" && {
-              borderRadius: Platform.OS == "web" ? 80 : ("50%" as any),
+          style={[
+            styles.tabButton,
+            activeTab === "all_groups" && {
+              borderRadius: 80,
               backgroundColor: colors.blue,
-            }),
-            alignItems: "center",
-            justifyContent: "center",
-          }}
+            },
+          ]}
         >
           <Text
-            style={{
-              fontSize: 16,
-              fontWeight: "400",
-              color: activeTab == "all_groups" ? colors.white : colors.bluegray,
-            }}
+            style={[
+              styles.tabButtonText,
+              {
+                color:
+                  activeTab === "all_groups" ? colors.white : colors.bluegray,
+              },
+            ]}
           >
             All Groups
           </Text>
@@ -91,48 +111,31 @@ export const StatusSection: FC<StatusSectionProps> = ({
             e.preventDefault();
             setActiveTab("archived");
           }}
-          style={{
-            flex: 1,
-            marginHorizontal: 4,
-            height: "100%",
-            ...(activeTab == "archived" && {
-              borderRadius: Platform.OS == "web" ? 80 : ("50%" as any),
+          style={[
+            styles.tabButton,
+            activeTab === "archived" && {
+              borderRadius: 80,
               backgroundColor: colors.blue,
-            }),
-            alignItems: "center",
-            justifyContent: "center",
-          }}
+            },
+          ]}
         >
           <Text
-            style={{
-              fontSize: 16,
-              fontWeight: "400",
-              color: activeTab == "archived" ? colors.white : colors.bluegray,
-            }}
+            style={[
+              styles.tabButtonText,
+              {
+                color:
+                  activeTab === "archived" ? colors.white : colors.bluegray,
+              },
+            ]}
           >
             Archived
           </Text>
         </TouchableOpacity>
-        <View
-          style={{
-            flex: 0.5,
-            marginHorizontal: 4,
-            height: "100%",
-            alignItems: "center",
-            justifyContent: "center",
-          }}
-        >
+        <View style={styles.filterButton}>
           <Feather name={"filter"} color={colors.bluegray} size={18} />
         </View>
       </View>
-      <View
-        style={{
-          flexDirection: "row",
-          marginTop: 12,
-          marginHorizontal: 8,
-          justifyContent: "space-between",
-        }}
-      >
+      <View style={styles.statusCardsContainer}>
         {statuses.map(
           (item: Status): ReactElement => (
             <StatusCard key={item.title} item={item} />
