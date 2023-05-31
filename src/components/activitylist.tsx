c914b464b2a254f9b4eca79d7c8faa045135cdd5
Fardeen Panjwani <fardeen.wi.panjwani@gmail.com>
2023-05-31 13:47:38 -0400

    feat: fully migrate ActivityList component's UI

--- src/components/activitylist.tsx
+++ src/components/activitylist.tsx
@@ -1,5 +1,5 @@
-import { FC, ReactElement, Component } from "react";
-import { View, Text } from "react-native";
+import { FC, ReactElement } from "react";
+import { View, Text, StyleSheet } from "react-native";
 import { MaterialCommunityIcons } from "@expo/vector-icons";
 
 import { useColors, Colors } from "../hooks/useColors";
@@ -22,94 +22,128 @@ export const ActivityCard: FC<ActivityCardProps> = (
   props: ActivityCardProps
 ): ReactElement => {
   const colors: Colors = useColors();
+  const styles = StyleSheet.create({
+    container: {
+      height: 140,
+      flex: 1,
+      backgroundColor: colors.white,
+      borderRadius: 12,
+      shadowColor: "#171717",
+      shadowOffset: { width: -1, height: 2 },
+      shadowOpacity: 0.1,
+      shadowRadius: 1,
+      maxWidth: "48%",
+      overflow: "hidden",
+      paddingHorizontal: 14,
+      justifyContent: "space-evenly",
+    },
+    titleContainer: {
+      width: "100%",
+      flex: 1,
+      justifyContent: "flex-end",
+    },
+    titleText: {
+      fontSize: 15,
+      fontWeight: "bold",
+    },
+    activityContainer: {
+      flex: 1,
+      width: "100%",
+      alignItems: "center",
+      flexDirection: "row",
+    },
+    callsContainer: {
+      flexDirection: "row",
+      alignItems: "flex-end",
+    },
+    callsValue: {
+      fontSize: 20,
+      marginRight: 4,
+      fontWeight: "bold",
+    },
+    callsLabel: {
+      fontSize: 14,
+      paddingBottom: 3,
+    },
+    deltaContainer: {
+      flexDirection: "row",
+      alignItems: "flex-end",
+      justifyContent: "flex-end",
+      marginLeft: 12,
+    },
+    deltaIcon: {
+      paddingBottom: 2,
+    },
+    deltaValue: {
+      fontSize: 14,
+      paddingBottom: 3,
+      marginLeft: 2,
+    },
+    averageContainer: {
+      width: "100%",
+      justifyContent: "flex-start",
+      flex: 1,
+    },
+    averageText: {
+      fontSize: 11,
+      color: colors.bluegray,
+    },
+  });
 
   return (
-    <View
-      style={{
-        height: 140,
-        flex: 1,
-        backgroundColor: colors.white,
-        borderRadius: 12,
-        shadowColor: "#171717",
-        shadowOffset: { width: -1, height: 2 },
-        shadowOpacity: 0.1,
-        shadowRadius: 1,
-        maxWidth: "48%",
-        overflow: "hidden",
-        paddingHorizontal: 14,
-        justifyContent: "space-evenly",
-      }}
-    >
-      <View
-        style={{
-          width: "100%",
-          flex: 1,
-          justifyContent: "flex-end",
-        }}
-      >
-        <H1 style={{ fontSize: 15, fontWeight: "bold" }}>
-          Activites this {props.item.duration}
-        </H1>
+    <View style={styles.container}>
+      <View style={styles.titleContainer}>
+        <H1 style={styles.titleText}>Activites this {props.item.duration}</H1>
       </View>
-      <View
-        style={{
-          flex: 1,
-          width: "100%",
-          alignItems: "center",
-          flexDirection: "row",
-        }}
-      >
-        <View style={{ flexDirection: "row", alignItems: "flex-end" }}>
+      <View style={styles.activityContainer}>
+        <View style={styles.callsContainer}>
           <Text
-            style={{
-              fontSize: 20,
-              color: props.item.deltaValue >= 0 ? colors.green : colors.crimson,
-              marginRight: 4,
-              fontWeight: "bold",
-            }}
+            style={[
+              styles.callsValue,
+              {
+                color:
+                  props.item.deltaValue >= 0 ? colors.green : colors.crimson,
+              },
+            ]}
           >
             {props.item.calls}
           </Text>
           <Text
-            style={{
-              fontSize: 14,
-              color: props.item.deltaValue >= 0 ? colors.green : colors.crimson,
-              paddingBottom: 3,
-            }}
+            style={[
+              styles.callsLabel,
+              {
+                color:
+                  props.item.deltaValue >= 0 ? colors.green : colors.crimson,
+              },
+            ]}
           >
             Calls
           </Text>
         </View>
-        <View
-          style={{
-            flexDirection: "row",
-            alignItems: "flex-end",
-            justifyContent: "flex-end",
-            marginLeft: 12,
-          }}
-        >
+        <View style={styles.deltaContainer}>
           <MaterialCommunityIcons
             name={`arrow-${
               props.item.deltaValue >= 0 ? "top-right" : "bottom-left"
             }-thin-circle-outline`}
-            style={{ paddingBottom: 2 }}
+            style={styles.deltaIcon}
             size={18}
             color={props.item.deltaValue >= 0 ? colors.green : colors.crimson}
           />
           <Text
-            style={{
-              fontSize: 14,
-              color: props.item.deltaValue >= 0 ? colors.green : colors.crimson,
-              paddingBottom: 3,
-              marginLeft: 2,
-            }}
+            style={[
+              styles.deltaValue,
+              {
+                color:
+                  props.item.deltaValue >= 0 ? colors.green : colors.crimson,
+              },
+            ]}
           >
             {props.item.deltaValue}%
           </Text>
         </View>
       </View>
-      <View style={{ width: "100%", justifyContent: "flex-start", flex: 1 }}>
-        <Text style={{ fontSize: 11, color: colors.bluegray }}>
+      <View style={styles.averageContainer}>
+        <Text style={styles.averageText}>
           Avg. {props.item.average.value} calls per{" "}
           {props.item.average.timePeriod}
         </Text>
