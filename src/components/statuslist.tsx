676eb36a0a5d48618a9bdb80bbbfb7b84e6b26ea
Fardeen Panjwani <fardeen.wi.panjwani@gmail.com>
2023-05-31 14:09:56 -0400

    feat: full migrate StatusCard component's UI

--- src/components/statuslist.tsx
+++ src/components/statuslist.tsx
@@ -1,5 +1,5 @@
 import { ReactElement, FC } from "react";
-import { Text, View } from "react-native";
+import { Text, View, StyleSheet } from "react-native";
 
 import { H1 } from ".";
 import { useColors, Colors } from "../hooks/useColors";
@@ -17,122 +17,96 @@ export const StatusCard: FC<StatusCardProps> = (
   props: StatusCardProps
 ): ReactElement => {
   const colors: Colors = useColors();
+  const styles = StyleSheet.create({
+    container: {
+      height: 184,
+      maxWidth: "47%",
+      flex: 1,
+      backgroundColor: colors.white,
+      borderRadius: 12,
+      shadowColor: "#171717",
+      shadowOffset: { width: -1, height: 2 },
+      shadowOpacity: 0.1,
+      shadowRadius: 1,
+      overflow: "hidden",
+      paddingHorizontal: 14,
+      justifyContent: "space-evenly",
+    },
+    titleContainer: {
+      width: "100%",
+      flex: 1,
+      justifyContent: "flex-end",
+    },
+    descriptionContainer: {
+      flex: 1,
+      width: "100%",
+      alignItems: "center",
+      flexDirection: "row",
+    },
+    descriptionText: {
+      fontSize: 14,
+      color: colors.bluegray,
+      fontWeight: "300",
+      paddingBottom: 3,
+    },
+    progressBar: {
+      height: 10,
+      borderRadius: 80,
+      width: "100%",
+      backgroundColor: colors.lightgray,
+    },
+    progressFill: {
+      height: "100%",
+      borderRadius: 80,
+      backgroundColor: colors.blue,
+    },
+    progressInfoContainer: {
+      flex: 1,
+      width: "100%",
+      flexDirection: "row",
+      paddingTop: 8,
+    },
+    progressDetailContainer: {
+      height: "100%",
+      flex: 1,
+    },
+    progressDetailTitle: {
+      fontSize: 12,
+      color: colors.bluegray,
+      fontWeight: "300",
+    },
+  });
 
   return (
-    <View
-      style={{
-        height: 184,
-        maxWidth: "47%",
-        flex: 1,
-        backgroundColor: colors.white,
-        borderRadius: 12,
-        shadowColor: "#171717",
-        shadowOffset: { width: -1, height: 2 },
-        shadowOpacity: 0.1,
-        shadowRadius: 1,
-        overflow: "hidden",
-        paddingHorizontal: 14,
-        justifyContent: "space-evenly",
-      }}
-    >
-      <View
-        style={{
-          width: "100%",
-          flex: 1,
-          justifyContent: "flex-end",
-        }}
-      >
+    <View style={styles.container}>
+      <View style={styles.titleContainer}>
         <H1>{props.item.title}</H1>
       </View>
-      <View
-        style={{
-          flex: 1,
-          width: "100%",
-          alignItems: "center",
-          flexDirection: "row",
-        }}
-      >
-        <Text
-          style={{
-            fontSize: 14,
-            color: colors.bluegray,
-            fontWeight: "300",
-            paddingBottom: 3,
-          }}
-        >
-          {props.item.description}
-        </Text>
+      <View style={styles.descriptionContainer}>
+        <Text style={styles.descriptionText}>{props.item.description}</Text>
       </View>
-      <View
-        style={{
-          width: "100%",
-          justifyContent: "flex-start",
-          flex: 1,
-        }}
-      >
+      <View style={styles.progressBar}>
         <View
-          style={{
-            height: 10,
-            borderRadius: 80,
-            width: "100%",
-            backgroundColor: colors.lightgray,
-          }}
-        >
-          <View
-            style={{
-              height: "100%",
-              borderRadius: 80,
+          style={[
+            styles.progressFill,
+            {
               width: `${
                 (props.item.completed /
                   (props.item.completed + props.item.pending)) *
                 100
               }%`,
-              backgroundColor: colors.blue,
-            }}
-          ></View>
+            },
+          ]}
+        ></View>
+      </View>
+      <View style={styles.progressInfoContainer}>
+        <View style={styles.progressDetailContainer}>
+          <H1>{props.item.completed}</H1>
+          <Text style={styles.progressDetailTitle}>Completed</Text>
         </View>
-        <View
-          style={{
-            flex: 1,
-            width: "100%",
-            flexDirection: "row",
-            paddingTop: 8,
-          }}
-        >
-          <View
-            style={{
-              height: "100%",
-              flex: 1,
-            }}
-          >
-            <H1>{props.item.completed}</H1>
-            <Text
-              style={{
-                fontSize: 12,
-                color: colors.bluegray,
-                fontWeight: "300",
-              }}
-            >
-              Completed
-            </Text>
-          </View>
-          <View
-            style={{
-              height: "100%",
-              flex: 1,
-            }}
-          >
-            <H1>{props.item.pending}</H1>
-            <Text
-              style={{
-                fontSize: 12,
-                color: colors.bluegray,
-                fontWeight: "300",
-              }}
-            >
-              Pending
-            </Text>
-          </View>
+        <View style={styles.progressDetailContainer}>
+          <H1>{props.item.pending}</H1>
+          <Text style={styles.progressDetailTitle}>Pending</Text>
         </View>
       </View>
     </View>
